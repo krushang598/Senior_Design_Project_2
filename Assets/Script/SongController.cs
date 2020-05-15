71687cf50e033f55bd0508c47ed0f87f24e91e57
Haoran He <hrgutou@gmail.com>
2020-05-15 14:50:06 -0400

    updates

---
+++ Assets/Script/SongController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongController : MonoBehaviour
+{
+    //references
+    private AudioSource mainAS;     //audio source playing given song
+
+    //variables
+    [SerializeField]
+    AudioClip playedSong;           //played song
+
+    public static float Volume = 0.4f;     //Volume of the audio source
+    public static float VolumeSound = 0.4f;
+
+    void Awake()
+    {
+        //getting audio source from gameobject
+        mainAS = GetComponent<AudioSource>();
+        //playing song
+        PlaySong(playedSong, Volume);
+    }
+
+    //function playing given song with given volume
+    public void PlaySong(AudioClip Song, float volume)
+    {
+        mainAS.volume = volume;
+        mainAS.clip = Song;
+        mainAS.Play();
+    }
+}
